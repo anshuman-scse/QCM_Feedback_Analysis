8adde9db23623ecfbbe1038211f391a8c3256dae
anshuman-scse <anshuman.scse@galgotiasuniversity.edu.in>
2026-06-04 15:02:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ if uploads:
 
     comparison_df = questions.copy()
     
-    fig, ax = plt.subplots(figsize=(11, 6))
+    fig, ax = plt.subplots(figsize=(11, 5))
     ax.tick_params( axis='x', labelsize=6 )
     colors = {
         "QCM-1": "orange",
